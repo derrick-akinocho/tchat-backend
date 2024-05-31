85b05ff763c662a511b1111781170a98532bff4f
Derrick <145935725+derrick-akinocho@users.noreply.github.com>
2024-05-31 08:46:05 +0200

    Initial commit

---
+++ src/main/java/com/ipi/tchatbackend/TchatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ipi.tchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TchatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TchatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipi/tchatbackend/TchatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipi.tchatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TchatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
